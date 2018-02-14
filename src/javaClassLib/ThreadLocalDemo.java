13729b7d85c429c34c1c321de1d7f9fc1585e8f5
Wang Xue <312759057@qq.com>
2018-02-14 10:53:20 +0800

    update ThreadLocal
    update ThreadLocal

--- src/javaClassLib/ThreadLocalDemo.java
+++ src/javaClassLib/ThreadLocalDemo.java
@@ -16,8 +16,24 @@ class Msg {
 }
 
 class MsgConsumer {
-	public void print(Msg msg) {
-		System.out.println(Thread.currentThread().getName() + " " + msg.getNote());
+	public void print() {
+		System.out.println(Thread.currentThread().getName() + " : " + MsgUtil.get().getNote());
+	}
+}
+
+class MsgUtil {
+	private static ThreadLocal<Msg> threadLocal = new ThreadLocal<Msg>();
+	
+	public static void set(Msg msg) {
+		threadLocal.set(msg);
+	}
+	
+	public static Msg get() {
+		return threadLocal.get();
+	}
+	
+	public static void remove() {
+		threadLocal.remove();
 	}
 }
 
@@ -27,12 +43,14 @@ public class ThreadLocalDemo {
 		new Thread(() -> {
 			Msg msg = new Msg();
 			msg.setNote("Hello world!");
-			new MsgConsumer().print(msg);
+			MsgUtil.set(msg);
+			new MsgConsumer().print();
 		}, "用户A").start();
 		new Thread(() -> {
 			Msg msg = new Msg();
 			msg.setNote("I don't give a shit!");
-			new MsgConsumer().print(msg);
+			MsgUtil.set(msg);
+			new MsgConsumer().print();
 		}, "用户B").start();
 	}
 
