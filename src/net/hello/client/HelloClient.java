6fb925b5ae24a20f4a4f0248515167fd546a886b
Wang Xue <312759057@qq.com>
2018-02-26 04:19:23 +0800

    update

--- src/net/hello/client/HelloClient.java
+++ src/net/hello/client/HelloClient.java
@@ -10,12 +10,12 @@ public class HelloClient {
 
 	public static void main(String[] args) throws Exception {
 		// 1.表示连接到指定的服务器端的主机名称和端口，localhost = 127.0.0.1
-		Socket client = new Socket("localhost", 9999);
+		Socket client = new Socket("localhost", 60004);
 		// 2.等待进行服务器端的输出，服务器端输出对客户端是输入
 		Scanner scan = new Scanner(client.getInputStream());
 		scan.useDelimiter("\n");
 		if(scan.hasNext()) {
-			System.out.println(scan.next());
+			System.out.println(scan.next().trim()); // 注意要去掉空格
 		}
 		client.close();
 	}
--- src/net/hello/server/HelloServer.java
+++ src/net/hello/server/HelloServer.java
@@ -11,7 +11,7 @@ public class HelloServer {
 
 	public static void main(String[] args) throws Exception {
 		// 1.创建一个服务器端的服务对象，所有的服务一定要有一个监听端口
-		ServerSocket server = new ServerSocket(9999); // 此时的服务在9999端口上等待连接
+		ServerSocket server = new ServerSocket(60004); // 此时的服务在9999端口上等待连接
 		System.out.println("等待客户来连接......");
 		// 2.需要等待客户连接，也就是说此时的程序在此处会进入到一个阻塞状态
 		Socket client = server.accept(); // 表示等待连接，连接的都是客户
